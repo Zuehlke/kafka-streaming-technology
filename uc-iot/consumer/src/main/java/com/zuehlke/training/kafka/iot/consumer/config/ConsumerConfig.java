2a358fa457b22b47d8f1d38ad17e5ca9248d8699
zzzk <s.stoeckli@gmail.com>
2024-03-12 23:50:27 +0100

    initial consumer

---
+++ uc-iot/consumer/src/main/java/com/zuehlke/training/kafka/iot/consumer/config/ConsumerConfig.java
@@ -0,0 +1,16 @@
+package com.zuehlke.training.kafka.iot.consumer.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.beans.BeanProperty;
+import java.beans.JavaBean;
+import java.util.List;
+
+@Configuration
+public class ConsumerConfig {
+
+   
+    
+}
